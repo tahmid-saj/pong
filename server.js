b72be898108be992ce96b5b3eb18d3abe7196f88
Tahmid Sajin <tahmid.saj@gmail.com>
2023-07-13 23:01:26 -0400

    Identifying users

--- server.js
+++ server.js
@@ -11,6 +11,6 @@ server.listen(PORT);
 console.log(`Listening on port ${PORT}`);
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
+  console.log('a user connected', socket.id);
 });
 
