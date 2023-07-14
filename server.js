dcf980d91ffdcf17d6d3897965ae9032d42000c7
Tahmid Sajin <tahmid.saj@gmail.com>
2023-07-14 01:06:16 -0400

    Connecting server to api

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
+const api = require("./api");
+
 const server = require("http").createServer();
+
+const sockets = require("./sockets");
+
 const io = require('socket.io')(server, {
   cors: {
     origin: '*',
@@ -10,31 +15,8 @@ const PORT = 3000;
 server.listen(PORT);
 console.log(`Listening on port ${PORT}`);
 
-let readyPlayerCount = 0;
+sockets.listen(io);
 
-io.on('connection', (socket) => {
-  console.log('a user connected', socket.id);
 
-  socket.on('ready', () => {
-    console.log("Player ready", socket.id);
 
-    readyPlayerCount++;
-
-    if (readyPlayerCount === 2) {
-      io.emit("startGame", socket.id);
-    };
-  });
-
-  socket.on("paddleMove", (paddleData) => {
-    socket.broadcast.emit('paddleMove', paddleData);
-  });
-
-  socket.on("ballMove", (ballData) => {
-    socket.broadcast.emit("ballMove", ballData);
-  });
-
-  socket.on("disconnect", (reason) => {
-    console.log(`Client ${socket.id} disconnected due to ${reason}`);
-  })
-});
 
